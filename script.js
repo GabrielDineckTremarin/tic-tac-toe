600c9330a478f4a792102bb44d45b872bbc85a86
Gabriel Dineck Tremarin <gabrieltremarin02@gmail.com>
2022-04-10 20:31:57 -0300

    fixing bug

--- script.js
+++ script.js
@@ -38,20 +38,24 @@ function checkElementAndAppend() {
 
         if(player1 == player2) {
             player1++;
-            
-            if(secondPlayer == 'ai-player') {
+            if(secondPlayer == 'ai-player' && !checkWinCondition()) {
 
-                    computerPlay()
-                    player2++;
+                computerPlay()
+                player2++;
             }
+
             
         } else {
             player2++;
         }
+
+        
        
     }
+
     checkWinCondition()
     
+
 }
 
 
@@ -105,6 +109,7 @@ function checkWinCondition() {
         if(b1Child == 'x' && b2Child == 'x' && b3Child == 'x'){
             declareWinner('x');
             j = 1;
+            return true
         } else if(b1Child == 'o' && b2Child == 'o' && b3Child == 'o'){
             declareWinner('o');
             j = 1;
@@ -123,6 +128,7 @@ function checkWinCondition() {
         if(b4Child == 'x' && b5Child == 'x' && b6Child == 'x'){
             declareWinner('x');
             j = 1;
+            return true
         } else if(b4Child == 'o' && b5Child == 'o' && b6Child == 'o'){
             declareWinner('o');
             j = 1;
@@ -141,6 +147,7 @@ function checkWinCondition() {
         if(b7Child == 'x' && b8Child == 'x' && b9Child == 'x'){
             declareWinner('x');
             j = 1;
+            return true
         } else if(b7Child == 'o' && b8Child == 'o' && b9Child == 'o'){
             declareWinner('o');
             j = 1;
@@ -162,6 +169,7 @@ function checkWinCondition() {
         if(b1Child == 'x' && b4Child == 'x' && b7Child == 'x'){
             declareWinner('x');
             j = 1;
+            return true
             
         } else if(b1Child == 'o' && b4Child == 'o' && b7Child == 'o'){
             declareWinner('o');
@@ -182,6 +190,7 @@ function checkWinCondition() {
         if(b2Child == 'x' && b5Child == 'x' && b8Child == 'x'){
             declareWinner('x');
             j = 1;
+            return true
         } else if(b2Child == 'o' && b5Child == 'o' && b8Child == 'o'){
             declareWinner('o');
             j = 1;
@@ -201,6 +210,7 @@ function checkWinCondition() {
         if(b3Child == 'x' && b6Child == 'x' && b9Child == 'x'){
             declareWinner('x');
             j = 1;
+            return true
         } else if(b3Child == 'o' && b6Child == 'o' && b9Child == 'o'){
             declareWinner('o');
             j = 1;
@@ -223,6 +233,7 @@ function checkWinCondition() {
 
             declareWinner('x');
             j = 1;
+            return true
         } else if(b1Child == 'o' && b5Child == 'o' && b9Child == 'o'){
             declareWinner('o');
             j = 1;
@@ -242,6 +253,7 @@ function checkWinCondition() {
         if(b3Child == 'x' && b5Child == 'x' && b7Child == 'x'){
             declareWinner('x');
             j = 1;
+            return true
         } else if(b3Child == 'o' && b5Child == 'o' && b7Child == 'o'){
             declareWinner('o');
             j = 1;
@@ -260,7 +272,7 @@ function checkWinCondition() {
         declareWinner('no one');
     }
  
-    
+    return false
 }
 
 //clean the board and update the scoreboard
@@ -339,11 +351,13 @@ function computerPlay() {
         if(counter == 0 && filled < 9){
             computerPlay()
         }
+
+        
         
     }
 
-
-
+    
+    
 }
 
 
